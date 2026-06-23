f6d66fc80ea71a961d7d0a00dd81525da6de1a83
TrishonP <trishonp1@gmail.com>
2026-06-22 19:28:30 -0600

    added projects app with views, urls, and template for projects list

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),  # Include the URLs from the pages app
+    path('', include('projects.urls'))
 
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def projects_list_view(request):
+    return render(request,'projects/projects_list.html')
