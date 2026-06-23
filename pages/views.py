c0d304708d789cc37eb8fa09198553a4cb0ee2a0
TrishonP <trishonp1@gmail.com>
2026-06-22 18:56:56 -0600

    added views/urls for experience, contacts, and projects.

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def about_me_view(request):
+    return render(request, 'pages/about.html')
+
+def experience_view(request):
+    return render(request, 'pages/experience.html')
+
+def contact_view(request):
+    return render(request, 'pages/contact.html')
+
+def projects_view(request):
+    return render(request, 'pages/projects.html')
